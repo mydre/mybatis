b39d24f9e42a8dc5bac9e590c5d017f5d60429a7
adog <adog@example.com>
2019-04-16 15:11:45 +0800

    解决jdbc的BatchUpdte问题，使用rewriteBatchedStatements参数，不能和mybatis使用的allowMultiQueries参数同时使用

--- src/main/java/TestSql.java
+++ src/main/java/TestSql.java
@@ -7,20 +7,21 @@ import java.sql.SQLException;
 
 public class TestSql {
 
+    private Connection conn = JdbcUtil.getConnection();
+    private PreparedStatement preparedStatement = null;
+    private String sql = "insert into person(username,email) values(?,?)";
+    private long startTime,endTime;
+
     @Test
     public void demo1(){
 
-        Connection conn = null;
-        PreparedStatement preparedStatement = null;
-        conn = JdbcUtil.getConnection();
-        JdbcUtil.begin( conn );
-        String sql = "insert into person(username,email) values(?,?)";
+        JdbcUtil.begin( conn );//set AutoCommit = False
         try {
             preparedStatement = conn.prepareStatement( sql );
-            long startTime,endTime;
+
             startTime = System.currentTimeMillis();
 
-            for(int i=0;i<1000;i++){
+            for(int i=0;i<10000;i++){
                 preparedStatement.setString( 1,"user" + (i + 1) );
                 preparedStatement.setString( 2,"adog@" + (i+1) + ".com" );
                 //这一步不能省略
@@ -28,10 +29,42 @@ public class TestSql {
             }
 
             JdbcUtil.commit( conn );
+            //关闭连接
+            JdbcUtil.closeResources( conn,preparedStatement,null );
             endTime = System.currentTimeMillis();
             System.out.println( endTime - startTime );
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+    @Test
+    public void testBatchUpdate(){
+
+        JdbcUtil.begin( conn );//set AutoCommit = False
+        try {
+            preparedStatement = conn.prepareStatement( sql );
+
+            startTime = System.currentTimeMillis();
+
+            for(int i=0;i<10000;i++){
+                preparedStatement.setString( 1,"user" + (i + 1) );
+                preparedStatement.setString( 2,"adog@" + (i+1) + ".com" );
+                preparedStatement.addBatch();
+                //这一步不能省略(进行批量更新)
+                if((i+1)%1000 == 0){
+                    preparedStatement.executeBatch();
+                    preparedStatement.clearBatch();
+                }
+            }
+
+            JdbcUtil.commit( conn );
+            //关闭连接
+            JdbcUtil.closeResources( conn,preparedStatement,null );
+            endTime = System.currentTimeMillis();
+            System.out.println( endTime - startTime );
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -39,3 +72,4 @@ public class TestSql {
     }
 
 }
+
